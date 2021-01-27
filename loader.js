e8024d11a898fcff0d85893e6554f976b071de30
jeffmer <jeffreymagee@gmail.com>
2021-01-27 21:14:11 +0000

    update loader

--- loader.js
+++ loader.js
@@ -4,7 +4,7 @@ if (window.location.host=="banglejs.com") {
 } else if (window.location.host=="espruino.github.io") {
   document.title += " [Development]";
   document.getElementById("apploaderlinks").innerHTML =
-    'This is the development Bangle.js App Loader - you can also try the <a href="https://banglejs.com/apps/">Official Version</a> for stable apps.';
+    'This is an Espruino App Loader fro an SMAQ3 smartwatch';
 } else {
   document.title += " [Unofficial]";
   document.getElementById("apploaderlinks").innerHTML =
@@ -22,9 +22,11 @@ var RECOMMENDED_VERSION = "2v08";
 })();
 
 function onFoundDeviceInfo(deviceId, deviceVersion) {
+/*
   if (deviceId != "BANGLEJS") {
     showToast(`You're using ${deviceId}, not a Bangle.js. Did you want <a href="https://espruino.com/apps">espruino.com/apps</a> instead?` ,"warning", 20000);
   } else if (versionLess(deviceVersion, RECOMMENDED_VERSION)) {
     showToast(`You're using an old Bangle.js firmware (${deviceVersion}). You can <a href="https://www.espruino.com/Bangle.js#firmware-updates" target="_blank">update with the instructions here</a>` ,"warning", 20000);
   }
+  */
 }
